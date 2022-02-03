2850c9357849a91d6ce8d4e1f8304d75d6d4172b
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-02-03 22:25:16 +0530

    Add some files

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,9 +17,7 @@ class TitleFragment : Fragment() {
     ): View? {
         val binding=DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
             R.layout.fragment_title,container,false)
-        return binding.playButton.setOnClickListener { view:View->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        return binding.root
 
     }
 }

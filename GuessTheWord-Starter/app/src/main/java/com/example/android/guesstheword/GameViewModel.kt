1729884971eda5b3cd2627182d9ed39449913c76
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-03-03 09:47:31 +0530

    Added some files

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/GameViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.guesstheword
-
-import androidx.lifecycle.ViewModel
-
-class GameViewModel:ViewModel() {
-    init{
-        Log.i("GameViewModel","GameViewModel called")
-    }
-}

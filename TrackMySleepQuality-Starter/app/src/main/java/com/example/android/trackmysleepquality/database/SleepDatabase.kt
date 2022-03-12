d69211334f0a1305a7dadc841012e70f4590aac2
Mohamedashiq301 <mohamedashiq301@gmail.com>
2022-03-12 22:12:26 +0530

    Added some files in track my sleep project

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -23,25 +23,30 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
 abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
     companion object {
+
         @Volatile
         private var INSTANCE: SleepDatabase? = null
-        fun getInstance(context:Context):SleepDatabase {
+
+        fun getInstance(context: Context): SleepDatabase {
             synchronized(this) {
                 var instance = INSTANCE
 
-            if (instance == null) {
-                instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    SleepDatabase::class.java,
-                    "sleep_history_database"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE=instance
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
             }
-            return instance
-        }
         }
     }
 }
